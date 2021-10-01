65e50395729c8d2a7cd6b3b76d77b3a0650b518f
EasonC13 <gtes9950406@gmail.com>
2021-10-02 07:34:41 +0800

    feat: setup env

---
+++ app.py
@@ -0,0 +1,61 @@
+
+from fastapi import FastAPI
+from starlette.exceptions import HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+import starlette.status as statusCode
+
+import asyncio
+
+
+app = FastAPI(title="NASA 2021")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
+from fastapi import APIRouter
+router = APIRouter()
+
+
+from pydantic import BaseModel
+from datetime import datetime
+import random
+import json
+
+
+from api.api_v1.api import router as api_v1_router
+
+app.include_router(api_v1_router, prefix="/api/v1", tags=["API"])
+
+
+from fastapi.responses import HTMLResponse, FileResponse
+import os
+
+static_file_path = "./static/"
+@app.get("/")
+def home():
+    with open(f"{static_file_path}/index.html") as f:
+        html = "".join(f.readlines())
+    return HTMLResponse(content=html, status_code= 200)
+
+@app.get("/{whatever:path}")
+async def get_static_files_or_404(whatever):
+    # try open file for path
+    file_path = os.path.join(static_file_path,whatever)
+    if os.path.isfile(file_path):
+        return FileResponse(file_path)
+    return FileResponse(f"{static_file_path}/index.html")
+
+
+if __name__ == "__main__":
+    import nest_asyncio
+    nest_asyncio.apply()
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=14001)
+
